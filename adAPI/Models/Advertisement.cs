73c401973b9173a1498fc652944000e9679bf3e1
MarkSmentina <mark.smentina@bk.ru>
2022-04-28 14:56:46 +0300

    Advertisement model has been added.

---
+++ adAPI/Models/Advertisement.cs
@@ -0,0 +1,11 @@
+﻿namespace adAPI.Models
+{
+    public class Advertisement
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+    }
+}
