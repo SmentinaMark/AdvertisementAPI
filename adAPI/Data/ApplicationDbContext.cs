3b43e03eff4337011f99eb68990e9676785e3d66
MarkSmentina <mark.smentina@bk.ru>
2022-04-28 14:55:11 +0300

    ApplicationDbContext has been added.

---
+++ adAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using adAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace adAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Advertisement> Advertisements { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
