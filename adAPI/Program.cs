d50a23a83315cc1311d28b2cef578dbd82e6dd43
MarkSmentina <mark.smentina@bk.ru>
2022-04-30 16:44:38 +0300

    Some services have been added.

--- adAPI/Program.cs
+++ adAPI/Program.cs
@@ -1,4 +1,8 @@
+using adAPI;
+using adAPI.Contracts;
 using adAPI.Data;
+using adAPI.Models;
+using adAPI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IDataManager<Advertisement>, DataManager>();
+builder.Services.AddScoped<IQueryManipulation<Advertisement,CollectionQueryParameters>, QueryManipulation>();
+
+builder.Services.AddMvc()
+       .AddNewtonsoftJson();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
