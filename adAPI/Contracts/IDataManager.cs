17fdde3c128e2567b00e8055b62722c35f314de8
MarkSmentina <mark.smentina@bk.ru>
2022-04-30 16:38:32 +0300

    IDataManager has been added.

---
+++ adAPI/Contracts/IDataManager.cs
@@ -0,0 +1,11 @@
+﻿using adAPI.Models;
+
+namespace adAPI.Contracts
+{
+    public interface IDataManager<T>
+    {
+        Task<IEnumerable<T>> GetItemsAsync(CollectionQueryParameters queryParameters);
+        T GetItemById(Guid id, bool additionalFields);
+        Task<T> AddItemAsync(T newItem);
+    }
+}
