51191825e80c05962a4c604f1c6b9555d30d3070
MarkSmentina <mark.smentina@bk.ru>
2022-04-30 16:36:17 +0300

    IQueryManipulation has been added.

---
+++ adAPI/Contracts/IQueryManipulation.cs
@@ -0,0 +1,10 @@
+﻿namespace adAPI.Contracts
+{
+    public interface IQueryManipulation<T,W>
+    {
+        IQueryable<T> SearchAdvertisements(W queryParameters, IQueryable<T> allAdvertisements);
+        IQueryable<T> PagingAdvertisements(W queryParameters, IQueryable<T> allAdvertisements);
+        IQueryable<T> SortAdvertisements(W queryParameters, IQueryable<T> allAdvertisements);
+        T GetAdditionalFields(bool additionalFields, T advertisement);
+    }
+}
