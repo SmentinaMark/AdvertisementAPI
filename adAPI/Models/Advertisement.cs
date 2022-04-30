4e07515202eed6d616661d49cbacce9ad40f398d
MarkSmentina <mark.smentina@bk.ru>
2022-04-30 16:45:39 +0300

    Advertisment model has been updated.

--- adAPI/Models/Advertisement.cs
+++ adAPI/Models/Advertisement.cs
@@ -1,11 +1,49 @@
-﻿namespace adAPI.Models
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace adAPI.Models
 {
     public class Advertisement
     {
+        [JsonIgnore]
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public double Price { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime CreationDate { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public bool IsDescriptionSerialize { get; set; }
+        [NotMapped]
+        [JsonIgnore]
+        public bool IsCreationDateSerialize { get; set; }
+
+        public bool ShouldSerializeDescription()
+        {
+            if(IsDescriptionSerialize)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ShouldSerializeCreationDate()
+        {
+            if (IsCreationDateSerialize)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
