26176a325c15a8c464659558085b5abecfe584e6
MarkSmentina <mark.smentina@bk.ru>
2022-05-03 12:27:23 +0300

    Revert "Revert "Advertisement validation have been added.""
    This reverts commit 3d79be2169911988008c3299426f7a4d3b710bba.

---
+++ adAPI.Tests/MockData/AdvertisementMockData.cs
@@ -0,0 +1,98 @@
+﻿using adAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adAPI.Tests.MockData
+{
+    public class AdvertisementMockData
+    {
+        List<Advertisement> advertisements = new List<Advertisement>
+        {
+                new Advertisement
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "01C19C72-31C3-4A85-85BD-CA8F99A10E11",
+                    Description = "FirstAdDescription",
+                    Price = 125,
+                    CreationDate = DateTime.Now 
+                },
+                new Advertisement
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "SecondAdTitle",
+                    Description = "SecondAdDescription",
+                    Price = 14124,
+                    CreationDate = DateTime.Now
+                },
+         };
+
+        public  List<Advertisement> GetAdvertisements()
+        {
+            return advertisements;
+        }
+
+        public List<Advertisement> GetEmptyAdvertisements()
+        {
+            return new List<Advertisement>();
+        }
+
+        public Advertisement GetSingleAdvertisement()
+        {
+            return new Advertisement
+            {
+                Id = Guid.Parse("01C19C72-31C3-4A85-85BD-CA8F99A10E18"),
+                Title = "FirstAdTitle",
+                Description = "FirstAdDescription",
+                Price = 125,
+                CreationDate = DateTime.Now
+            };
+        }
+
+        public Advertisement AddCorrectAdvertisement()
+        {
+            Advertisement advertisement = new Advertisement
+            {
+                Id = Guid.NewGuid(),
+                Title = "ThirdAdTitle",
+                Description = "ThirdAdDescription",
+                Price = 125, 
+                CreationDate = DateTime.Now 
+            
+            };
+            advertisements.Add(advertisement);
+            return advertisement;
+        }
+
+        public Advertisement AddEmptyAdvertisement()
+        {
+            return new Advertisement();
+        }
+
+        public CollectionQueryParameters SetCollectionParamaters()
+        {
+            CollectionQueryParameters parameters = new CollectionQueryParameters
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                Search = "",
+                Sort = ""
+            };
+            return parameters;
+        }
+
+        public CollectionQueryParameters SetCollectionParamatersWithSearch()
+        {
+            CollectionQueryParameters parameters = new CollectionQueryParameters
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                Search = "First",
+                Sort = ""
+            };
+            return parameters;
+        }
+    }
+}
--- adAPI/Program.cs
+++ adAPI/Program.cs
@@ -3,32 +3,46 @@ using adAPI.Contracts;
 using adAPI.Data;
 using adAPI.Models;
 using adAPI.Services;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("adAPI")));
 
-builder.Services.AddScoped<IDataManager<Advertisement>, DataManager>();
-builder.Services.AddScoped<IQueryManipulation<Advertisement,CollectionQueryParameters>, QueryManipulation>();
+builder.Services.AddScoped<IDataManager<Advertisement, CollectionQueryParameters>, DataManager>();
+builder.Services.AddScoped<IQueryManipulation<Advertisement, CollectionQueryParameters>, QueryManipulation>();
 
-builder.Services.AddMvc()
-       .AddNewtonsoftJson();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddFluentValidation(fv =>
+    {
+        fv.ImplicitlyValidateChildProperties = true;
+        fv.ImplicitlyValidateRootCollectionElements = true;
 
-builder.Services.AddControllers();
+        fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(x =>
+{
+    x.SwaggerEndpoint("/swagger/v1/swagger.json", "ad API V1");
+    x.RoutePrefix = String.Empty;
+    });
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 app.UseHttpsRedirection();
