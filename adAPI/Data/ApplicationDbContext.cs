5e63c93d78806204ecd94f42a971665566d30870
MarkSmentina <mark.smentina@bk.ru>
2022-05-03 12:21:09 +0300

    Property bind have been added.

--- adAPI/Data/ApplicationDbContext.cs
+++ adAPI/Data/ApplicationDbContext.cs
@@ -3,12 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace adAPI.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
         public DbSet<Advertisement> Advertisements { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder ModelBuilder)
+        {
+            ModelBuilder.Entity<Advertisement>()
+            .Property(b => b._Images).HasColumnName("Images");
         }
     }
 }
