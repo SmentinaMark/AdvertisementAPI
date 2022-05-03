217d75dca55b61bec65b3e71fc21ff809a45a216
MarkSmentina <mark.smentina@bk.ru>
2022-05-03 16:12:30 +0300

    Summary has been updated.

--- adAPI/Contracts/IDataManager.cs
+++ adAPI/Contracts/IDataManager.cs
@@ -8,14 +8,14 @@
     public interface IDataManager<T, W>
     {
         /// <summary>
-        /// The method allows you to get a list of items from the database context.
+        /// The method allows to get a list of items from the database context.
         /// </summary>
         /// <param name="queryParameters">An object that includes the parameters necessary for sorting, pagination, and search.</param>
         /// <returns>The final list of elements, edited depending on the passed parameters.</returns>
         List<T> GetItems(W queryParameters);
 
         /// <summary>
-        /// The method allows you to get item by ID from the database context.
+        /// The method allows to get item by ID from the database context.
         /// </summary>
         /// <param name="id">ID of the element to be found in the database context.</param>
         /// <param name="additionalFields">Flag responsible for enabling additional fields.</param>
@@ -23,7 +23,7 @@
         T GetItemById(Guid id, bool additionalFields);
 
         /// <summary>
-        /// Allows you to add a new object to the database.
+        /// Allows to add a new object to the database.
         /// </summary>
         /// <param name="newItem">The object to be added to the database.</param>
         /// <returns>Added object.</returns>
