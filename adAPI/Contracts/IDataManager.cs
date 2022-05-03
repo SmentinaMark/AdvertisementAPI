1e9cba93f2d8d5640ba762bdfccae19c84bbde42
MarkSmentina <mark.smentina@bk.ru>
2022-05-03 12:16:53 +0300

    Summary have been added.

--- adAPI/Contracts/IDataManager.cs
+++ adAPI/Contracts/IDataManager.cs
@@ -1,11 +1,32 @@
-﻿using adAPI.Models;
-
-namespace adAPI.Contracts
+﻿namespace adAPI.Contracts
 {
-    public interface IDataManager<T>
+    /// <summary>
+    /// Defines a contract that represents interaction with data.
+    /// </summary>
+    /// <typeparam name="T">Entity data type.</typeparam>
+    /// <typeparam name="W">Parameters data type.</typeparam>
+    public interface IDataManager<T, W>
     {
-        Task<IEnumerable<T>> GetItemsAsync(CollectionQueryParameters queryParameters);
+        /// <summary>
+        /// The method allows you to get a list of items from the database context.
+        /// </summary>
+        /// <param name="queryParameters">An object that includes the parameters necessary for sorting, pagination, and search.</param>
+        /// <returns>The final list of elements, edited depending on the passed parameters.</returns>
+        List<T> GetItems(W queryParameters);
+
+        /// <summary>
+        /// The method allows you to get item by ID from the database context.
+        /// </summary>
+        /// <param name="id">ID of the element to be found in the database context.</param>
+        /// <param name="additionalFields">Flag responsible for enabling additional fields.</param>
+        /// <returns>Found object</returns>
         T GetItemById(Guid id, bool additionalFields);
-        Task<T> AddItemAsync(T newItem);
+
+        /// <summary>
+        /// Allows you to add a new object to the database.
+        /// </summary>
+        /// <param name="newItem">The object to be added to the database.</param>
+        /// <returns>Added object.</returns>
+        T AddItem(T newItem);
     }
 }
