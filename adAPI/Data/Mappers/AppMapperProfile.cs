a26b6d8b17be57ccb722df2ac3a22e109a745660
MarkSmentina <mark.smentina@bk.ru>
2022-05-07 14:26:10 +0300

    AutoMapperProfile has been added.

---
+++ adAPI/Data/Mappers/AppMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using adAPI.Contracts;
+using adAPI.Models;
+using AutoMapper;
+
+namespace adAPI.Data.Mappers
+{
+    public class AppMapperProfile:Profile
+    {
+        public AppMapperProfile()
+        {
+            CreateMap<Advertisement, GetAdvertisements>();
+            CreateMap<Advertisement, GetSingleAdvertisement>().ForMember(x => x._Images, opt => opt.Ignore()); ;
+
+            CreateMap<Advertisement, CreateAdvertisement>();
+        }
+    }
+}
