fd5b25e9b7cc7d49dcf002c23488031a86d0533f
MarkSmentina <mark.smentina@bk.ru>
2022-05-07 14:43:43 +0300

    IAdvertisementService has been added.

---
+++ adAPI/Services/Interfaces/IAdvertisementService.cs
@@ -0,0 +1,11 @@
+﻿using adAPI.Models;
+
+namespace adAPI.Services
+{
+    public interface IAdvertisementService
+    {
+        List<Advertisement> GetItems(CollectionQueryParameters queryParameters);
+        Advertisement GetItemById(Guid id, bool additionalFields);
+        Advertisement AddItem(Advertisement newItem);
+    }
+}
