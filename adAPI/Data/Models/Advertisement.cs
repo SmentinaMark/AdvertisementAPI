7988c7668e421097d5a0411dbeaa00f509a0e6bb
MarkSmentina <mark.smentina@bk.ru>
2022-05-07 14:26:21 +0300

    Model has been added.

--- adAPI/Data/Models/Advertisement.cs
+++ adAPI/Data/Models/Advertisement.cs
@@ -5,7 +5,6 @@ namespace adAPI.Models
 {
     public class Advertisement
     {
-        [JsonIgnore]
         public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
@@ -13,115 +12,14 @@ namespace adAPI.Models
         [Column(TypeName = "Date")]
         public DateTime CreationDate { get; set; }
 
-        #region Images
-        /// <summary>
-        /// This field returns first item from array of Images[].
-        /// </summary>
-        public string MainImage
-        {
-            get
-            {
-                if (!string.IsNullOrWhiteSpace(_Images))
-                {
-                    return Images[0];
-                }
-                else
-                {
-                    return string.Empty;
-                }
+        public string Images { get; set; } = string.Empty;
 
-            }
-        }
 
-        /// <summary>
-        /// Array of items returns deseriazided items from _Images and sets serialized value into _Image.
-        /// </summary>
         [NotMapped]
-        public string[] Images
-        {
-            get
-            {
-                return string.IsNullOrWhiteSpace(_Images) ? Array.Empty<string>() : JsonConvert.DeserializeObject<string[]>(_Images);
-            }
-            set
-            {
-                _Images = JsonConvert.SerializeObject(value);
-            }
-        }
-
-        /// <summary>
-        /// This field is bind with the db column.
-        /// </summary>
-        [JsonIgnore]
-        public string _Images { get; set; } = string.Empty;
-        #endregion
-
-        #region ShouldSerialize
-        /// <summary>
-        /// Flag for displaying the description.
-        /// </summary>
-        [NotMapped]
-        [JsonIgnore]
         public bool IsDescriptionSerialize { get; set; }
-        public bool ShouldSerializeDescription()
-        {
-            if (IsDescriptionSerialize)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Flag for displaying the date.
-        /// </summary>
         [NotMapped]
-        [JsonIgnore]
         public bool IsCreationDateSerialize { get; set; }
-        public bool ShouldSerializeCreationDate()
-        {
-            if (IsCreationDateSerialize)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Flag for displaying all images.
-        /// </summary>
         [NotMapped]
-        [JsonIgnore]
         public bool IsGetAllImages { get; set; }
-        public bool ShouldSerializeImages()
-        {
-            if (IsGetAllImages)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool ShouldSerializeMainImage()
-        {
-            if (!IsGetAllImages)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        #endregion
     }
 }
