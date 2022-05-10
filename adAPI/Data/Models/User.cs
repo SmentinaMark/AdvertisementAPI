4bbaffb97f8ae363668b2906a22317d57d8edddb
MarkSmentina <mark.smentina@bk.ru>
2022-05-10 10:52:57 +0300

    User model has been added.

---
+++ adAPI/Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace adAPI.Data.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = "SimpleUser";
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
