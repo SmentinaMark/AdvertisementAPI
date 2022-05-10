ed4acdc443866b77efc5e0c863af06273a7e5d9d
MarkSmentina <mark.smentina@bk.ru>
2022-05-10 10:53:47 +0300

    UserLogin, UserRegister contracts have been added.

---
+++ adAPI/Contracts/Requests/Users/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace adAPI.Contracts.Requests
+{
+    public class UserLogin
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
